782adf75da6849007b83e6d5084fce7c8dcd05a3
Jakub Buczyński <72788108+BuczekB@users.noreply.github.com>
2023-09-11 12:03:52 +0200

    add scss and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 
-import './App.css'
+import './App.scss'
+
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 import FetchData from './container/FetchData.jsx'
+import Navigation from './container/Navigation'
+
+import About from './container/About'
+import Shop from './container/Shop'
+import Contact from './container/Contact'
 
 function App() {
 
   return (
     <>
+   <Router>
+   <Navigation></Navigation>
+    <Routes>
+    <Route path='/' exact element={<About/>} />
+    <Route path='/About' exact element={<About/>} />
+    <Route path='/Shop' exact element={<Shop/>} />
+    <Route path='/Contact' exact element={<Contact/>} />
+    <Route>404 Not Found</Route>
+    </Routes>
     <FetchData></FetchData>
+   </Router>
     </>
   )
 }
---
+++ src/container/Navigation.jsx
@@ -0,0 +1,19 @@
+
+import {Link} from 'react-router-dom'
+
+import '../Style/Navigation.scss'
+
+function Navigation() {
+
+  return (
+    <nav>
+       <ul>
+        <Link to='/About'><li>About</li></Link>
+        <Link to='/Shop'><li>Shop</li></Link>
+        <Link to='/Contact'><li>Contact</li></Link>
+       </ul>
+    </nav>
+  )
+}
+
+export default Navigation

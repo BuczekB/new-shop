1c49f910fa483306f5ab9d8f94cf70be765c3c6b
Jakub Buczyński <72788108+BuczekB@users.noreply.github.com>
2023-10-11 16:32:04 +0200

    add content in about page

--- src/container/About.jsx
+++ src/container/About.jsx
@@ -4,7 +4,11 @@ function About() {
 
     return (
       <div>
-        <h1>About</h1>         
+        <h1>FakeShop is my project when i simulate problems and solutions like we meet in normal comercial ecommers website.</h1> 
+
+        <h2>To build frontend version I use React and SCSS, to build backend NodeJS but backend is simple app to communicate with DataBase. Most of the application is frontend</h2>
+           
+        <h2>To login as a Admin use "admin123"</h2>     
       </div>
     )
   }

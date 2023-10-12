72050b4beb17aeb8b6145a134a75ae3b00b1007c
Jakub Buczyński <72788108+BuczekB@users.noreply.github.com>
2023-10-12 14:55:07 +0200

    add readme informations

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 Shop Project
 
-# To run project open console and write 'npm run dev'
+# To run project open console and write 'npm run dev' in folder backend to run backend and do this some in main folder "new-shop'
+
+# To login as a admin use login and password "admin123"
+
+# When you login as an admin you get new permissions like add new product and delete existing products
--- src/container/About.jsx
+++ src/container/About.jsx
@@ -8,7 +8,9 @@ function About() {
 
         <h2>To build frontend version I use React and SCSS, to build backend NodeJS but backend is simple app to communicate with DataBase. Most of the application is frontend</h2>
            
-        <h2>To login as a Admin use "admin123"</h2>     
+        <h2>To login as a Admin use "admin123"</h2>  
+
+        <h2>When you login as an admin you get new permissions like add new product and delete existing products</h2>   
       </div>
     )
   }

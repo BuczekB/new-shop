7ff00192949a48c2152b10a71d351cf7658696f6
Jakub Buczyński <72788108+BuczekB@users.noreply.github.com>
2024-05-12 22:10:45 +0200

    adedd input to sort items in shop by price

--- src/container/Shop.jsx
+++ src/container/Shop.jsx
@@ -13,6 +13,7 @@ function Shop() {
 
    const [isTruOrFalse, setIsTruOrFalse] = useState(false)
    const [singleItem, setSingleItem] = useState({})
+   const [sortedProducts, setSortedProducts] = useState([])
 
    const products =  useSelector((state) => state.allProducts.products)
 
@@ -22,11 +23,31 @@ function Shop() {
     setSingleItem(payload)
    }
 
+  const sortProductByPrice = (e) => {
+    
+    
+    const value = e.target.value
+
+    if(value === 'highest'){
+      const sortedProducts = [...products].sort((a,b) => a.price - b.price)
+    setSortedProducts(sortedProducts)
+    }
+    if(value === 'lowest'){
+      const sortedProducts = [...products].sort((a,b) => b.price - a.price)
+    setSortedProducts(sortedProducts)
+    }
+  }
+
+  useEffect(() =>{
+    setSortedProducts(products)
+  },[products])
+
+
   
     
 
    
-    const listOfProducts =  products.map( (item) =>{
+    const listOfProducts =  sortedProducts.map( (item) =>{
       return(
         <Card key={item._id} id={item._id} name={item.name} price={item.price} image={item.image} quantity={item.quantity} open={openInfoWindow}/>
       )
@@ -34,11 +55,15 @@ function Shop() {
 
 
 
-    console.log(listOfProducts);
+   
 
 
     return (
-      <div className="shopContainer">
+      <div className="shopContainer" >
+          <select onClick={sortProductByPrice} className='shopContainerFilter' >
+          <option  value="highest">Price from highest</option>
+          <option  value="lowest">Price from lowest</option>
+          </select>
           <InfoAboutBuy isTruOrFalse={isTruOrFalse} openInfoWindow={openInfoWindow} singleItem={singleItem} ></InfoAboutBuy>
           {listOfProducts.length ? listOfProducts : <h1>Loading...</h1>}
       </div>
